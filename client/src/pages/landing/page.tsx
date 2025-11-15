765a10a96efc846156d8bce34c2b60783ad7fa31
hatem12345th <h.taleb@esi-sba.dz>
2025-11-15 11:54:59 +0100

    landing page final verison

--- client/src/pages/landing/page.tsx
+++ client/src/pages/landing/page.tsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
 
 const LandingPage = () => {
-  return (
-    <div>LandingPage</div>
-  )
-}
+  const [loading, setLoading] = useState(true);
 
-export default LandingPage
+  return (
+    <div className="relative h-screen w-full">
+      {loading && (
+        <div className="absolute inset-0 flex items-center justify-center bg-white z-10">
+          <p className="text-lg font-semibold">Loading...</p>
+        </div>
+      )}
+
+      <iframe
+        src="https://column-sample-08321954.figma.site/"
+        className="h-screen w-full"
+        onLoad={() => setLoading(false)}
+      ></iframe>
+    </div>
+  );
+};
+
+export default LandingPage;
